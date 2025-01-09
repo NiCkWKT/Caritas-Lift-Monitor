bc7d3b644121374ce76c563e334e8eac3a68bd04
nickwkt <nickb55245@gmail.com>
2025-01-10 00:34:39 +0800

    feat: add build config

--- src/electron/main.js
+++ src/electron/main.js
@@ -23,7 +23,6 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1920,
     height: 1080,
-    backgroundColor: "#ccc",
     fullscreen: false,
     frame: false,
     webPreferences: {
